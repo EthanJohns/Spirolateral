083f0023bd35bbce29e2ddf9ea7a9934426b81cc
48418153 <Ethan_Johns@paraparaumucollege.school.nz>
2019-03-13 11:05:26 +1300

    First Update, Turtle Test to establish some base code CDice is cannibalizing an old program that used TK for a GUI.

---
+++ CDice.py
@@ -0,0 +1,75 @@
+from tkinter import *
+import turtle
+from random import randint
+CDtrayOpen = False
+
+
+class Application(Frame):
+
+    def __init__(self, master):
+        super().__init__(master)
+        self.grid()
+        
+        self.TurtleScreen = turtle.Screen()
+        vcmd = (master.register(self.validate_float),
+                '%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
+        self.create_widgets(vcmd)
+
+    def create_widgets(self,vcmd):
+        self.SpiroBot = turtle.Turtle()
+        
+        self.quit = Button(self, text="QUIT", fg="red", command= lambda:[root.destroy(), self.TurtleScreen.bye()])
+        self.quit.grid(row=0, column=7)
+
+        self.hi_there = Button(
+            self, text="Do you want a cup holder", fg="blue", command=self.cupholder)
+        self.hi_there.grid(row=0, column=5)
+
+        
+        self.Distancentry = Entry(self, width = 10, validate = 'key', validatecommand = vcmd,)
+        self.Distancentry.insert(0,'1')
+        self.Distancentry.grid(row = 1, column = 5, sticky = W)        
+        
+        
+        
+        
+        self.d4 = Button(self, text='Move Forward ', font=("Comic Sans MS", 11),command=self.TurtleMove)
+        self.d4.grid(row=0, column=1)
+
+
+        self.output = Label(root, text="")
+        self.output.grid()
+
+    def cupholder(self):
+        global CDtrayOpen
+        if CDtrayOpen == False:
+            ctypes.windll.WINMM.mciSendStringW(
+                u"set cdaudio door open", None, 0, None)
+            CDtrayOpen = True
+        else:
+            ctypes.windll.WINMM.mciSendStringW(
+                u"set cdaudio door closed", None, 0, None)
+            CDtrayOpen = False
+    
+    def TurtleMove(self):
+        self.SpiroBot.fd(int(self.Distancentry.get()))
+
+    def validate_float(self, action, index, value_if_allowed,
+    prior_value, text, validation_type, trigger_type, widget_name):
+        # action=1 -> insert
+        if(action=='1'):
+            if text in '0123456789.-+':
+                try:
+                    float(value_if_allowed)
+                    return True
+                except ValueError:
+                    return False
+            else:
+                return False
+        else:
+            return True
+    
+
+root = Tk()
+app = Application(root)
+app.mainloop()
---
+++ turtleTest.py
@@ -0,0 +1,8 @@
+'''Testing the GUI limitations of Turtle'''
+import turtle               # allows us to use the turtles library
+wn = turtle.Screen()        # creates a graphics window
+alex = turtle.Turtle()      # create a turtle named alex
+alex.forward(150)           # tell alex to move forward by 150 units
+alex.left(90)               # turn by 90 degrees
+alex.forward(75)            # complete the second side of a rectangle
+wn.exitonclick()
