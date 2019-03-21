2a0234a60dce9c639cf19bff5c07c47b24dbed72
48418153 <Ethan_Johns@paraparaumucollege.school.nz>
2019-03-22 12:26:24 +1300

    It works, thank

--- CDice.py
+++ CDice.py
@@ -2,8 +2,9 @@
 from tkinter import *
 import turtle
 
+
 class Spirolateral:
-    def __init__(self, name, segment, angle):
+    def __init__(self, name: str, segment: int, angle):
         self.name = name
         self.segment = segment
         self.angle = angle
@@ -19,18 +20,18 @@ class Application(Frame):
         self.PX = 20
         self.PY = 10
 
-        self.homeframe = Frame(master, width=400, height=320)
-        self.homeframe.grid_propagate(0)
-        self.homeframe.grid()
+        self.__homeframe = Frame(master, width=400, height=320)
+        self.__homeframe.grid_propagate(0)
+        self.__homeframe.grid()
 
-        self.displaying_header = Frame(self.homeframe,  bg=self.BG_COL,
+        self.displaying_header = Frame(self.__homeframe,  bg=self.BG_COL,
                                        width=400, height=60)
         self.displaying_header.grid_propagate(0)
         self.displaying_header.grid(row=0, columnspan=2)
 
-        self.inputframe = Frame(master, width=400, height=320)
-        self.inputframe.grid_propagate(0)
-        self.collecting_header = Frame(self.inputframe,  bg=self.BG_COL,
+        self.__inputframe = Frame(master, width=400, height=320)
+        self.__inputframe.grid_propagate(0)
+        self.collecting_header = Frame(self.__inputframe,  bg=self.BG_COL,
                                        width=400, height=60)
         self.collecting_header.grid_propagate(0)  # preserves the space we want
         self.collecting_header.grid(row=0, columnspan=2)
@@ -39,8 +40,6 @@ class Application(Frame):
                                  text="Collecting Spiro Data")
         collecting_label.grid(row=0, column=0,  padx=self.PX, pady=self.PY)
 
-       
-        
         self.vcmd = (master.register(self.validate_float),
                      '%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
 
@@ -49,98 +48,106 @@ class Application(Frame):
         self.create_home_widgets()
         self.create_input_widgets()
         '''
-        self.canvas = Canvas(self.homeframe, width = 400, height = 400)
+        self.canvas = Canvas(self.__homeframe, width = 400, height = 400)
         self.canvas.grid(row = 8, column = 0)
         self.SpiroBot = turtle.RawTurtle(self.canvas)
         '''
+
     def create_home_widgets(self):
-        self.quit = Button(self.displaying_header, text="QUIT", fg="red", command=root.destroy)
-        self.quit.grid(row=0, column=2)
         '''
-        self.movefd = Button(self.homeframe, text='Move Forward ', font=(
-            "Comic Sans MS", 11), command=self.TurtleMove)
-        self.movefd.grid(row=10, column=1)
+        self.quit = Button(self.displaying_header, text="QUIT",
+                           fg="red", command=root.destroy)
+        self.quit.grid(row=0, column=2)
         '''
         displaying_label = Label(
             self.displaying_header, bg=self.BG_COL, anchor=NW, text="Displaying Spiro Data")
         displaying_label.grid(row=0, column=0, sticky=NW,  padx=20, pady=10)
 
         self.go_to_collect_btn = Button(
-            self.displaying_header, width=15, anchor=NW, text="Add New Spiro", command=self.inputGrid)
-        self.go_to_collect_btn.grid(row=0, column=1, sticky=NW,  padx=self.PX,
+            self.displaying_header, width = 10, text="Add Spiro", command=self.inputGrid)
+        self.go_to_collect_btn.grid(row=0, column=1, padx=self.PX,
                                     pady=self.PY)
 
-        fname_label_d = Label(self.homeframe, anchor=NW,
+        fname_label_d = Label(self.__homeframe, anchor=NW,
                               text="Spiro name:")
         fname_label_d.grid(row=1, column=0, sticky=NW, padx=self.PX,
-                           pady=self.PY/2)
+                           pady=self.PY)
 
-        self.first_name = Label(self.homeframe, anchor=NW)
-        self.first_name.grid(row=1, column=1, sticky=NW, pady=self.PY/2)
+        self.first_name = Label(self.__homeframe, anchor=NW)
+        self.first_name.grid(row=1, column=1, sticky=NW, pady=self.PY)
 
-        age_label_d = Label(self.homeframe, anchor=NW, text="Segment: ")
+        age_label_d = Label(self.__homeframe, anchor=NW, text="Segment: ")
         age_label_d.grid(row=2, column=0, sticky=NW,
-                         padx=self.PX, pady=self.PY/2)
+                         padx=self.PX, pady=self.PY)
 
-        self.age = Label(self.homeframe, anchor=NW)
-        self.age.grid(row=2, column=1, sticky=NW, pady=5)
+        self.age = Label(self.__homeframe, anchor=NW)
+        self.age.grid(row=2, column=1, sticky=NW, pady=self.PY)
 
-        angel_label_d = Label(self.homeframe,anchor=NW, text="Angel:")
-        angel_label_d.grid(row=3,column=0,sticky=NW,padx =self.PX,pady=self.PY/2)
+        angel_label_d = Label(self.__homeframe, anchor=NW, text="Angel:")
+        angel_label_d.grid(row=3, column=0, sticky=NW,
+                           padx=self.PX, pady=self.PY)
 
-        self.angel = Label(self.homeframe, anchor=NW)
-        self.angel.grid(row=3,column=1,sticky=NW, pady=5)
+        self.angel = Label(self.__homeframe, anchor=NW)
+        self.angel.grid(row=3, column=1, sticky=NW, pady=self.PY)
 
-        self.mobile_info = Label(self.homeframe)
+        self.mobile_info = Label(self.__homeframe)
         self.mobile_info.grid(row=4, columnspan=2, sticky=W+E)
 
-        self.prev_btn = Button(self.homeframe, text="Previous",
+        self.prev_btn = Button(self.__homeframe, text="Previous",
                                state=DISABLED, command=self.previous)
         self.prev_btn.grid(row=5, column=0, sticky=W,
                            padx=self.PX/2, pady=self.PY)
 
-        self.next_btn = Button(self.homeframe, text="Next",
+        self.next_btn = Button(self.__homeframe, text="Next",
                                state=DISABLED, command=self.next_person)
-        self.next_btn.grid(row=5, column=1, sticky=E, padx=self.PX/2,pady=self.PY)
-        self.index = 0
-    def create_input_widgets(self):
-        self.quit = Button(self.collecting_header, text="QUIT", fg="red", command=lambda: root.destroy())
-        self.quit.grid(row=0, column=2)
+        self.next_btn.grid(row=5, column=1, sticky=E,
+                           padx=self.PX/2, pady=self.PY)
+        self.__index = 0
 
+    def create_input_widgets(self):
+        '''
+        self.quit = Button(self.collecting_header, text="QUIT",
+                           fg="red", command=lambda: root.destroy())
+        self.quit.grid(row=0, column=2)
+        '''
         self.go_to_display_btn = Button(
             self.collecting_header, width=10,  text="Show All", command=self.homeGrid)
         self.go_to_display_btn.grid(
             row=0, column=1, padx=self.PX, pady=self.PY)
 
-        fname_label = Label(self.inputframe, anchor=NW,
+        fname_label = Label(self.__inputframe, anchor=NW,
                             text="Spiro Name:")
         fname_label.grid(row=1, column=0, sticky=NW,
                          padx=self.PX, pady=self.PY)
 
-        self.spiro_name_entry = Entry(self.inputframe,validate = 'key', validatecommand = self.vcmd)
-        self.spiro_name_entry.grid(row=1, column=1, sticky=NW, pady=self.PY/2)
+        self.spiro_name_entry = Entry(self.__inputframe)
+        self.spiro_name_entry.grid(row=1, column=1, sticky=NW, pady=self.PY)
 
-        segment_label = Label(self.inputframe, anchor=NW, text="Segment:")
+        segment_label = Label(self.__inputframe, anchor=NW, text="Segment:")
         segment_label.grid(row=2, column=0, sticky=NW,
-                           padx=self.PX, pady=self.PY/2)
+                           padx=self.PX, pady=self.PY)
 
-        self.age_entry = Entry(self.inputframe,validate = 'key', validatecommand =self.vcmd)
+        self.age_entry = Entry(
+            self.__inputframe, validate='key', validatecommand=self.vcmd)
         self.age_entry .grid(row=2, column=1, sticky=NW)
 
-        angel_label = Label(self.inputframe, anchor=NW, text="Angle:")
+        angel_label = Label(self.__inputframe, anchor=NW, text="Angle:")
         angel_label.grid(row=3, column=0, sticky=NW,
-                         padx=self.PX, pady=self.PY/2)
+                         padx=self.PX, pady=self.PY)
 
-        self.angel_entry = Entry(self.inputframe,validate = 'key', validatecommand =self.vcmd)
+        self.angel_entry = Entry(
+            self.__inputframe, validate='key', validatecommand=self.vcmd)
         self.angel_entry .grid(row=3, column=1, sticky=NW)
 
-        self.create_person_btn = Button(self.inputframe, width=10,
+        self.create_person_btn = Button(self.__inputframe, width=10,
                                         text="Enter Data",
                                         command=self.make_spiro)
         self.create_person_btn.grid(row=5, columnspan=2, pady=15)
+        self.errorLabel = Label(self.__inputframe, text="")
+        self.errorLabel.grid(row=6,columnspan = 2)
 
     def TurtleMove(self):
-        self.spirolateralist[self.index].segment
+        self.spirolateralist[self.__index].segment
 
     def validate_float(self, action, index, value_if_allowed,
                        prior_value, text, validation_type, trigger_type, widget_name):
@@ -158,26 +165,30 @@ class Application(Frame):
 
     def inputGrid(self):
         """ Switches from collecting frame to displaying frame.  """
-        
-        
-        self.homeframe.grid_forget()
-        self.inputframe.grid_propagate(0)
-        self.inputframe.grid(row=0, column=0)
+        self.__homeframe.grid_forget()
+        self.__inputframe.grid_propagate(0)
+        self.__inputframe.grid(row=0, column=0)
 
     def homeGrid(self):
-        self.inputframe.grid_forget()
-        self.homeframe.grid_propagate(0)
-        self.homeframe.grid(row=0, column=0)
+        self.__inputframe.grid_forget()
+        self.__homeframe.grid_propagate(0)
+        self.__homeframe.grid(row=0, column=0)
 
         if len(self.spirolateralist) > 0:
-            self.index = 0
+            self.__index = 0
             self.show_data()
         else:
-           self.mobile_info.configure(text = "There is not currently any data to show")
+            self.mobile_info.configure(
+                text="There is not currently any data to show")
 
     def make_spiro(self):
-        self.spirolateralist.append(Spirolateral(self.spiro_name_entry.get(),
-                                                 self.age_entry.get(), self.angel_entry.get()))
+        if self.spiro_name_entry.get() or self.age_entry.get() or self.angel_entry.get() == "":
+            self.errorLabel.configure(text="There is no data entered.")
+            print(1)
+        else:
+            spiroClass = Spirolateral(self.spiro_name_entry.get(), self.age_entry.get(), self.angel_entry.get())
+            self.spirolateralist.append(spiroClass)
+
         if len(self.spirolateralist) > 1:
             self.next_btn.configure(state=NORMAL)
         self.clear()
@@ -189,23 +200,23 @@ class Application(Frame):
         self.angel_entry.delete(0, END)
 
     def next_person(self):
-        """ Increments self.index and calls show_data method. Disables next
+        """ Increments self.__index and calls show_data method. Disables next
             button if at the end of the list. Ensures prev button is normal.
         """
-        self.index += 1
-        
-        if self.index == len(self.spirolateralist)-1:
+        self.__index += 1
+
+        if self.__index == len(self.spirolateralist)-1:
             self.next_btn.configure(state=DISABLED)
 
         self.prev_btn.configure(state=NORMAL)
         self.show_data()
 
     def previous(self):
-        """ Decrements self.index and calls show_data method. Disables prev
+        """ Decrements self.__index and calls show_data method. Disables prev
             button if at the start of the list. Ensures next button is normal.
         """
-        self.index -= 1
-        if self.index == 0:
+        self.__index -= 1
+        if self.__index == 0:
             self.prev_btn.configure(state=DISABLED)
 
         self.next_btn.configure(state=NORMAL)
@@ -213,11 +224,12 @@ class Application(Frame):
 
     def show_data(self):
         """ Configures the displaying frame to show data associated with person
-            object stored at self.index
+            object stored at self.__index
         """
-        self.first_name.configure(text=self.spirolateralist[self.index].name)
-        self.age.configure(text=self.spirolateralist[self.index].segment)
-        self.angel.configure(text="{}°".format(self.spirolateralist[self.index].angle))
+        self.first_name.configure(text=self.spirolateralist[self.__index].name)
+        self.age.configure(text=self.spirolateralist[self.__index].segment)
+        self.angel.configure(text="{}°".format(
+            self.spirolateralist[self.__index].angle))
 
 
 root = Tk()
